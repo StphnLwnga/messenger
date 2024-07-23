b2872845435ee93dffb559b8096155e06b6c5753
StphnLwnga <boywilder99@gmail.com>
2024-07-23 16:36:40 +0200

    UI - minor changes to login

--- messenger-client/app/(site)/components/auth-form.tsx
+++ messenger-client/app/(site)/components/auth-form.tsx
@@ -1,13 +1,14 @@
 "use client"
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { useTheme } from "next-themes"
 import { BsGithub } from "react-icons/bs";
-import { FcGoogle, FcRefresh } from "react-icons/fc";
+import { FcGoogle } from "react-icons/fc";
 import axios from 'axios';
 import { signIn, useSession } from 'next-auth/react'
+import { LiaSpinnerSolid } from "react-icons/lia";
 
 import { cn } from '@/lib/utils';
 import { Input } from "@/components/ui/input"
@@ -15,9 +16,15 @@ import { Button } from '@/components/ui/button';
 import { useToast } from "@/components/ui/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import { AuthSocialButton } from './auth-social-button';
+import { Loading } from '@/components/ui/loading';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
+/**
+ * Renders the authentication form component.
+ *
+ * @return {JSX.Element} The rendered authentication form component.
+ */
 export default function AuthForm(): JSX.Element {
   const session = useSession();
   const router = useRouter();
@@ -30,6 +37,26 @@ export default function AuthForm(): JSX.Element {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
 
+  useEffect(() => {
+    console.log(session)
+    if (session?.status === 'authenticated') {
+      toast({
+        title: 'Logged in!',
+        variant: 'default',
+        className: `${resolvedTheme === 'dark' ? 'bg-emerald-500' : 'bg-emerald-500 text-gray-100'} border-0 border-gray-200`,
+      });
+      router.push('/users');
+    }
+  }, [session?.status, router]);
+
+  /**
+   * Toggles the variant between 'LOGIN' and 'REGISTER'.
+   *
+   * This function clears any errors, resets the form, and updates the variant state.
+   * It checks the current variant and sets it to the opposite value.
+   *
+   * @return {void} This function does not return a value.
+   */
   const toggleVariant = () => {
     clearErrors();
     reset();
@@ -50,6 +77,12 @@ export default function AuthForm(): JSX.Element {
     },
   });
 
+  /**
+   * Handles the form submission for login or registration.
+   *
+   * @param {FieldValues} data - The form data containing name, email, and password.
+   * @return {Promise<void>} A promise that resolves when the submission is complete.
+   */
   const onSubmit: SubmitHandler<FieldValues> = async (data): Promise<void> => {
     setIsLoading(true);
     setIsSubmitted(false);
@@ -60,7 +93,7 @@ export default function AuthForm(): JSX.Element {
         ...data,
         redirect: false,
       }).then(callback => {
-        console.log(callback)
+        // console.log(callback)
         if (callback?.error) {
           toast({
             title: 'Login failed',
@@ -74,6 +107,7 @@ export default function AuthForm(): JSX.Element {
               </ToastAction>
             ),
           });
+          router.push('/users');
         }
         if (callback?.ok && !callback?.error) {
           toast({
@@ -110,12 +144,15 @@ export default function AuthForm(): JSX.Element {
           headers: { 'Content-Type': 'application/json', }
         });
 
-        console.log(res)
+        // console.log(res)
         const { user, code, message } = res.data?.data?.createUserFromCredentials;
         if (code !== 200 && message.includes("email")) throw new Error(`Email already exists`);
         if (code !== 200) throw new Error(`Registration failed`);
         setIsSubmitted(true);
-        return user;
+        await signIn('credentials', {
+          email,
+          password,
+        });
       } catch (error: any) {
         console.log(error);
         toast({
@@ -138,7 +175,13 @@ export default function AuthForm(): JSX.Element {
     }
   }
 
-  const socialAction = async (action: string) => {
+  /**
+   * Executes a social action by signing in with the specified action.
+   *
+   * @param {string} action - The action to be performed.
+   * @return {Promise<void>} A promise that resolves when the action is completed.
+   */
+  const socialAction = async (action: string): Promise<void> => {
     setIsLoading(true);
     setIsSubmitted(false);
     try {
@@ -158,121 +201,119 @@ export default function AuthForm(): JSX.Element {
   }
 
   useEffect(() => {
-    console.log(session)
-    if (session?.status === 'authenticated') {
-      toast({
-        title: 'Logged in!',
-        variant: 'default',
-        className: `${resolvedTheme === 'dark' ? 'bg-emerald-500' : 'bg-emerald-500 text-gray-100'} border-0 border-gray-200`,
-      });
-      router.push('/users');
-    }
-  }, [session?.status]);
-
-  useEffect(() => {
     isSubmitted && reset();
   }, [isSubmitted]);
 
-  return (
-    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
-      <div className={cn(
-        "px-4 py-8 shadow shadow-gray-400 bg-white sm:rounded-lg sm:px-10",
-        resolvedTheme === 'dark' && "shadow-gray-500 bg-gray-800",
-      )}>
-        <form
-          className='space-y-6'
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          {variant === 'REGISTER' && (
-            <Input
-              disabled={isLoading}
-              id="name"
-              type="text"
-              label='Name'
-              required
-              placeholder='George Jetson'
-              register={register}
-              errors={errors}
-            />
-          )}
-          <Input
-            disabled={isLoading}
-            id="email"
-            type="email"
-            label='Email'
-            required
-            placeholder='spaced@example.com'
-            register={register}
-            errors={errors}
-          />
-          <Input
-            disabled={isLoading}
-            id="password"
-            type="password"
-            label="Password"
-            required
-            placeholder='********'
-            register={register}
-            errors={errors}
-          />
-          <div className='pt-4'>
-            <Button
-              disabled={isLoading}
-              type="submit"
-              variant="secondary"
-              fullWidth
-              className={cn(
-                'signin text-gray-100 text-md',
-              )}
-            >
-              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
-            </Button>
-          </div>
-        </form>
-
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div
-                className={cn(
-                  "w-full border-t border-gray-300",
-                  resolvedTheme === 'dark' && "border-gray-500",
-                )}
-              />
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className={cn(
-                "px-2",
-                "bg-white text-gray-500",
-                resolvedTheme === 'dark' && "bg-gray-800 text-gray-300",
-              )}>
-                Or continue with
-              </span>
-            </div>
-          </div>
-
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
-            <AuthSocialButton icon={FcGoogle} onClick={() => socialAction('google')} />
-          </div>
-
-          <div className={cn(
-            "flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500",
-            resolvedTheme === 'dark' && "text-gray-300",
-          )}>
-            <div className="font-bold">
-              {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
-            </div>
-            <div
-              onClick={toggleVariant}
-              className="underline cursor-pointer"
-            >
-              {variant === 'LOGIN' ? 'Create an account' : 'Login to your account'}
-            </div>
-          </div>
-
+  switch (true) {
+    case session?.status === 'loading' || session?.status === 'authenticated':
+      return (
+        <div className='flex w-full items-center justify-center align-middle fixed pr-[4%]'>
+          <Loading resolvedTheme={resolvedTheme} size={188} />
         </div>
-      </div>
-    </div>
-  )
+      );
+
+    default:
+      return (
+        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+          <div className={cn(
+            "px-4 py-8 shadow shadow-gray-400 bg-white sm:rounded-lg sm:px-10",
+            resolvedTheme === 'dark' && "shadow-gray-500 bg-gray-800",
+          )}>
+            <form
+              className='space-y-6'
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {variant === 'REGISTER' && (
+                <Input
+                  disabled={isLoading}
+                  id="name"
+                  type="text"
+                  label='Name'
+                  required
+                  placeholder='George Jetson'
+                  register={register}
+                  errors={errors}
+                />
+              )}
+              <Input
+                disabled={isLoading}
+                id="email"
+                type="email"
+                label='Email'
+                required
+                placeholder='spaced@example.com'
+                register={register}
+                errors={errors}
+              />
+              <Input
+                disabled={isLoading}
+                id="password"
+                type="password"
+                label="Password"
+                required
+                placeholder='********'
+                register={register}
+                errors={errors}
+              />
+              <div className='pt-4'>
+                <Button
+                  disabled={isLoading}
+                  type="submit"
+                  variant="secondary"
+                  fullWidth
+                  className={cn(
+                    'signin text-gray-100 text-md',
+                  )}
+                >
+                  {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                </Button>
+              </div>
+            </form>
+
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div
+                    className={cn(
+                      "w-full border-t border-gray-300",
+                      resolvedTheme === 'dark' && "border-gray-500",
+                    )}
+                  />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className={cn(
+                    "px-2",
+                    "bg-white text-gray-500",
+                    resolvedTheme === 'dark' && "bg-gray-800 text-gray-300",
+                  )}>
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 flex gap-2">
+                <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
+                <AuthSocialButton icon={FcGoogle} onClick={() => socialAction('google')} />
+              </div>
+
+              <div className={cn(
+                "flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500",
+                resolvedTheme === 'dark' && "text-gray-300",
+              )}>
+                <div className="font-bold">
+                  {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+                </div>
+                <div
+                  onClick={toggleVariant}
+                  className="underline cursor-pointer"
+                >
+                  {variant === 'LOGIN' ? 'Create an account' : 'Login to your account'}
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      );
+  }
 }
--- messenger-client/app/(site)/page.tsx
+++ messenger-client/app/(site)/page.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import { useTheme } from "next-themes"
+import { useSession } from 'next-auth/react'
 
 import { cn } from "@/lib/utils"
 import AuthForm from "./components/auth-form";
 
 export default function Home() {
+  const session = useSession();
   const { resolvedTheme } = useTheme();
 
   return (
@@ -17,17 +19,22 @@ export default function Home() {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
-          height={48}
-          width={48}
-          className="mx-auto w-auto h-auto"
+          height={session?.status === 'unauthenticated' ? 48 : 42}
+          width={session?.status === 'unauthenticated' ? 48 : 42}
+          className={cn(
+            "mx-auto w-auto h-auto",
+            session?.status === 'unauthenticated' && 'mr-[29%] pr-6'
+          )}
           src="/images/rocket.png"
         />
-        <h2 className={cn(
-          "mt-6 text-center text-3xl font-bold tracking-tight",
-          // resolvedTheme === 'light' && "text-gray-900",
-        )}>
-          Sign in to your account
-        </h2>
+        {session?.status === 'unauthenticated' && (
+          <h2 className={cn(
+            "mt-6 text-center text-3xl font-bold tracking-tight",
+            // resolvedTheme === 'light' && "text-gray-900",
+          )}>
+            Sign in to your account
+          </h2>
+        )}
       </div>
       <AuthForm />
     </div>
