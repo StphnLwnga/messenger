475e7fcedf47b8ba02af12c88bfd5f891a2c789e
StphnLwnga <boywilder99@gmail.com>
2024-07-24 00:22:03 +0200

    client - minor change

--- messenger-client/components/providers/apollo-provider.tsx
+++ messenger-client/components/providers/apollo-provider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 const graphql_endpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!
 
@@ -8,6 +8,14 @@ const client = new ApolloClient({
   uri: graphql_endpoint,
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   ssrMode: true,
+//   link: createHttpLink({
+//     uri: graphql_endpoint,
+//   }),
+//   cache: new InMemoryCache(),
+// });
 
 export function ApolloProviderWrapper({
   children,
